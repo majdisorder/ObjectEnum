a4831380df3cba6450f49326e21fd060c34483f9
Dimitri Troncquo <dimitri.troncquo@gmail.com>
2019-10-14 15:15:03 +0200

    add unit tests

---
+++ System.ValueEnum.Tests/Models/TestEnum.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.ValueEnum.Tests.Models
+{
+    [ExcludeFromCodeCoverage]
+    internal abstract class TestEnum : ValueEnum<TestEnum.Value>
+    {
+        public enum Value
+        {
+            Unknown = 0,
+            First = 1,
+            Second = 2,
+            Third = 3
+        }
+
+        protected TestEnum(Value value)
+            : base(value)
+        {
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal abstract class AltTestEnum : ValueEnum<AltTestEnum.Value>
+    {
+        public enum Value
+        {
+            Unknown = 0,
+            First = 1,
+            Second = 2,
+            Third = 3
+        }
+
+        protected AltTestEnum(Value value)
+            : base(value)
+        {
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal class FirstEnum : TestEnum
+    {
+        public FirstEnum(Value value)
+            : base(value)
+        {
+        }
+
+        private static readonly IReadOnlyCollection<Value> Values = new[] {Value.Unknown, Value.First};
+
+        protected override IReadOnlyCollection<Value> GetDefinedValues()
+            => Values;
+    }
+    
+    [ExcludeFromCodeCoverage]
+    internal class AltFirstEnum : AltTestEnum
+    {
+        public AltFirstEnum(Value value)
+            : base(value)
+        {
+        }
+
+        private static readonly IReadOnlyCollection<Value> Values = new[] {Value.Unknown, Value.First};
+
+        protected override IReadOnlyCollection<Value> GetDefinedValues()
+            => Values;
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal class SecondEnum : TestEnum
+    {
+        public SecondEnum(Value value)
+            : base(value)
+        {
+        }
+
+        private static readonly IReadOnlyCollection<Value> Values = new[] {Value.Unknown, Value.Second};
+
+        protected override IReadOnlyCollection<Value> GetDefinedValues()
+            => Values;
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal class CombinedEnum : TestEnum
+    {
+        public CombinedEnum(Value value)
+            : base(value)
+        {
+        }
+
+        private static readonly IReadOnlyCollection<Value> Values = new[] {Value.Unknown, Value.First, Value.Second};
+
+        protected override IReadOnlyCollection<Value> GetDefinedValues()
+            => Values;
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal class DerivedCombinedEnum : CombinedEnum
+    {
+        public DerivedCombinedEnum(Value value)
+            : base(value)
+        {
+        }
+
+        private static readonly IReadOnlyCollection<Value> Values = new[]
+            {Value.Unknown, Value.First, Value.Second, Value.Third};
+
+        protected override IReadOnlyCollection<Value> GetDefinedValues()
+            => Values;
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal abstract class AbstractDerivedCombinedEnum : CombinedEnum
+    {
+        protected AbstractDerivedCombinedEnum(Value value)
+            : base(value)
+        {
+        }
+
+        private static readonly IReadOnlyCollection<Value> Values = new[]
+            {Value.Unknown, Value.First, Value.Second, Value.Third};
+
+        protected override IReadOnlyCollection<Value> GetDefinedValues()
+            => Values;
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal class ConcreteDerivedCombinedEnum : CombinedEnum
+    {
+        public ConcreteDerivedCombinedEnum(Value value)
+            : base(value)
+        {
+        }
+    }
+}
