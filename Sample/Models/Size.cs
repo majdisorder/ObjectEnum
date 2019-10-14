e2eb439c667c80395b81353b8b71b9b5cee2cc30
Dimitri Troncquo <dimitri.troncquo@gmail.com>
2019-10-14 22:29:09 +0200

    add some examples to Sample project

---
+++ Sample/Models/Size.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sample.Models
+{
+    public abstract class Size : ValueEnum<Size.Value>
+    {
+        public enum Value
+        {
+            Unknown = 0,
+            ExtraSmall = 1,
+            Small = 2,
+            Medium = 3,
+            Large = 4,
+            ExtraLarge = 5
+        }
+
+        protected Size(Value value)
+            : base(value)
+        {
+        }
+    }
+}
