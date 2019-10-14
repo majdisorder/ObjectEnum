44877c310cd5a0ad06596dbaea9635dd2e854d54
Dimitri Troncquo <dimitri.troncquo@gmail.com>
2019-10-14 22:29:09 +0200

    add some examples to Sample project

---
+++ Sample/Models/LargeSize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Sample.Models
+{
+    public class LargeSize : Size
+    {
+        public LargeSize(Value value)
+            : base(value)
+        {
+        }
+
+        private static readonly Value[] Values = new[]
+        {
+            Value.Unknown, Value.ExtraLarge, Value.Large
+        };
+
+        protected override IReadOnlyCollection<Value> GetDefinedValues()
+            => Values;
+    }
+}
---
+++ Sample/Models/WeekDay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sample.Models
+{
+    public class WeekDay : ValueEnum<DayOfWeek>
+    {
+        public WeekDay(DayOfWeek value)
+            : base(value) { }
+
+        private static readonly DayOfWeek[] Values = new[]
+        {
+            DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday,
+            DayOfWeek.Thursday, DayOfWeek.Friday
+        };
+
+        protected override IReadOnlyCollection<DayOfWeek> GetDefinedValues()
+            => Values;
+    }
+}
